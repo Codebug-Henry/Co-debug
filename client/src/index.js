d77ed3fae07c7975f9165e92d480fab79fc037a7
Regenerik <regenerik@gmail.com>
2022-06-21 09:16:31 -0300

    Update react 17 to 18.2-logo diferente-index adaptado a react 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 // import { Provider } from "react-redux"
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import ReactDOMClient from "react-dom/client"
 import App from './App';
 import store from "./redux/store"
 import { Auth0Provider } from "@auth0/auth0-react"
@@ -11,12 +12,15 @@ const {
 const domain = REACT_APP_AUTH0_DOMAIN;
 const clientId = REACT_APP_AUTH0_CLIENT_ID;
 
-ReactDOM.render(
+const container = document.getElementById("root")
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <Auth0Provider store={store} domain={domain} clientId={clientId} redirectUri={window.location.origin}>
     {/* <Provider store={store}> */}
       <App />
     {/* </Provider> */}
-  </Auth0Provider>,
-  document.getElementById('root')
+  </Auth0Provider>//,
+//  document.getElementById('root')
 );
 
