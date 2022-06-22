05e3225649429b32c6ae457ad70cd8bf84a9654e
Parricio <patriciogabriel colella@gmail.com>
2022-06-21 23:55:07 -0300

    controllers/routes question

--- api/src/controllers/questionControllers.js
+++ api/src/controllers/questionControllers.js
@@ -1,33 +1,96 @@
-const deleteUserQuestion=(req,res,next)=>{
-   
-}
+const {User,Question} = require("../db.js")
 
-const putUserQuestion=(req,res,next)=>{
-   
-}
+//en caso de comprobar hardcodeando descomentar lineas, 55-56-51-61-62
+//y la constante user_try
+//y comentar lineas 25-31
+//esto devuelve la question creada y el usuario al que fue agregada
 
-const postQuestion=(req,res,next)=>{
-   
-}
-
-// const PostQuestion=(req,res,next)=>{
-//     const sub = req.params.sub
-//     const newQuestion = req.body
-
-//     User.findByPk(sub)
-//     .then(User_finded=>
-//         Question.create(newQuestion)
-//         .then(Q_Created=>{
-//             Q_Created.setUser(User_finded)
-//             User_finded.addQuestion(Q_Created)
-//             .then(()=>res.send(Q_Created))
-//             }
-//         )
-//     ).catch(e=>next(e))
+// const user_try={
+//     sub:"id 1",
+//     name:"usuario de prueba",
+//     email:"usuario.com",
+//     locale:"argnetina",
 // }
 
-const getSingleQuestion=(req,res,next)=>{
+const deleteUserQuestion=async (req,res,next)=>{
+    const {id} = req.params
 
+    try {
+        await Question.destroy({
+            where:{
+                id
+            }
+        })
+        res.send(await User.findOne({include:Question}))
+    } catch (error) {
+        next(error)
+    }
+}
+
+const putUserQuestion=async (req,res,next)=>{
+    const {id,text,title} = req.body
+
+    try {
+        await Question.update({text,title},{
+            where:{
+                id:parseInt(id)
+            }
+        })
+        let questionUpdated=await Question.findByPk(id)
+
+        res.send({
+            text:questionUpdated.text,
+            title:questionUpdated.title
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+//les dijimos que manden el id del usuario, pero no es necesario
+
+
+const postQuestion = async (req,res,next)=>{
+   const {sub,text,title} = req.body
+
+//    let user 
+
+   if(!sub||!text||!title) next(new Error(message="falta informacion"))
+   try {
+        // user = await User.findByPk(user_try.sub)
+        // if(!user)user = await  User.create(user_try)
+        const user = await User.findByPk(sub)
+        const newQuestion = await Question.create({text,title})
+        await user.addQuestion(newQuestion)
+        newQuestion.setUser(user)
+        // const user_whit_question=await User.findAll({include:Question})
+        // res.send([newQuestion,user_whit_question])
+        res.send(newQuestion)
+
+   } catch (error) {
+        next(error)
+   }
+}
+
+const getSingleQuestion=async (req,res,next)=>{
+    const id = req.params.id
+
+    try {
+        let questionFound = await Question.findByPk(id)
+        let user=await questionFound.getUser()
+
+        res.send(
+            {
+                user:{
+                    name:user.name,
+                },
+                ...questionFound.dataValues,
+            }
+        )
+
+    } catch (error) {
+        next(error)
+    }
 }
 
 module.exports={
