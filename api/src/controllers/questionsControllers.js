c2c7a4055a0473657fd67f237cbe287b128cb96d
Matias <matibalbi@gmail.com>
2022-06-22 10:02:11 -0300

    questions contr. updated, general contr. created

--- api/src/controllers/questionsControllers.js
+++ api/src/controllers/questionsControllers.js
@@ -1,16 +1,58 @@
-const getAllQuestions=(req,res,next)=>{
-    const {search,sort}=req.query
+const { Question } = require("../db");
+const { Op } = require('sequelize');
+const { sortQuestionsAsc, sortQuestionsDesc } = require("./generalControllers");
 
-    // if(search) //filtra por search
-    // else if( sort) //amoda por siort
-    // else finall
+const getAllQuestions= async (req,res,next) => {
+
+    const {search, sort} = req.query
+
+    try {
+
+        let condition = search
+        ? {[Op.or]: [{ title: {[Op.iLike]: `%${search}%`} }, { text: {[Op.iLike]: `%${search}%`} }]}
+        : {}
+        
+        let allQuestions = await Question.findAll({where: condition})
+        
+        if (sort === "ascendent") allQuestions.sort(sortQuestionsAsc)
+        else if (sort === "descendent") allQuestions.sort(sortQuestionsDesc)
+
+        res.send(allQuestions)
+
+    } catch (error) {
+        next(error)
+    }
 
 }
 
-const getUserQuestions=(req,res,next)=>{
-    // por query answered=boolean
-    //if(answered===undefined) todo
-    // else uno u otro
+const getUserQuestions= async (req,res,next) => {
+
+    const {sub} = req.params
+    const {answered} = req.query
+
+    try {
+
+        let condition = {userSub: sub}
+
+        switch (answered) {
+            case "true":
+                condition = {...condition, cantAnswers: {[Op.gt]: 0}}
+                break
+            case "false":
+                condition = {...condition, cantAnswers: 0}
+                break
+            default:
+                break
+        }
+        
+        let myQuestions = await Question.findAll({where: condition})
+        
+        res.send(myQuestions)
+
+    } catch (error) {
+        next(error)
+    }
+    
 }
 
 const putFavourites=(req,res,next)=>{
