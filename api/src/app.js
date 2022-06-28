4e7c10c16fb8fcd3fa73b17c2fdffb1021f65049
Felipe Guitelmane <felipe.guitelman@.com>
2022-06-27 21:25:59 -0300

    cambios para poder deployar

--- api/src/app.js
+++ api/src/app.js
@@ -2,20 +2,22 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const routes = require('./routes/index');
+const routes = require('./routes/index.js');
 
 require('./db.js');
 
 const server = express();
+const cors = require('cors')
 
 server.name = 'API';
 
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,19 @@ import React from 'react';
 import ReactDOMClient from "react-dom/client"
 import App from './App';
 import store from "./redux/store"
+import axios from 'axios';
 import { Auth0Provider } from "@auth0/auth0-react"
 require('dotenv').config();
 const {
-  REACT_APP_AUTH0_DOMAIN, REACT_APP_AUTH0_CLIENT_ID
+  REACT_APP_AUTH0_DOMAIN, REACT_APP_AUTH0_CLIENT_ID, REACT_APP_API
 } = process.env;
 const domain = REACT_APP_AUTH0_DOMAIN;
 const clientId = REACT_APP_AUTH0_CLIENT_ID;
 
 const container = document.getElementById("root")
 const root = ReactDOMClient.createRoot(container);
+
+axios.defaults.badeURL = REACT_APP_API || "http://localhost:3001"
 
 root.render(
   <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
