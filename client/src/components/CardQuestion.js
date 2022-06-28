2910511fe7e45ca780e9ec449203887a51aa1923
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-06-28 09:16:22 -0300

    Modificaciones estilos

--- client/src/components/CardQuestion.js
+++ client/src/components/CardQuestion.js
@@ -1,54 +1,58 @@
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
-import React from 'react'
-import style from "./styles/CardQuestion.module.css"
-import like from "../images/like2.png"
-import dislike from "../images/dislike2.png"
-import denuncia from "../images/denuncia2.png"
-import favorito from "../images/favorito2.png"
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import React from "react";
+import style from "./styles/CardQuestion.module.css";
+import like from "../images/like2.png";
+import dislike from "../images/dislike2.png";
+import denuncia from "../images/denuncia2.png";
+import favorito from "../images/favorito2.png";
 
-
-
-const CardQuestion = ({cantAnswers, nickname, picture, likes, title, text, teachPoints,id}) => {
-
-   const [ likeOnScreen, setlikeOnScreen ] = useState(likes)
+const CardQuestion = ({
+  cantAnswers,
+  nickname,
+  picture,
+  likes,
+  title,
+  text,
+  teachPoints,
+  id,
+}) => {
+  const [likeOnScreen, setlikeOnScreen] = useState(likes);
 
   return (
     <div className={`container-fluid ${style.total}`}>
-      <div className={`row gy-300 ${style.fila}`}>
-        <div className={`col-2-lg ${style.pictureBox}`}>
+      <div className={`row ${style.fila}`}>
+        <div className={`col-lg-2 ${style.pictureBox}`}>
           <img
             className={style.userImage}
             src={picture}
             alt="imagen de usuario"
           />
         </div>
-        <div className={`col-8-lg ${style.leftBox}`}>
+        <div className={`col-lg-8 ${style.leftBox}`}>
           <div className={style.TitleAndExtrasBox}>
-              <div className={style.userPreg}>
-                <h6>{nickname} pregunta:</h6>
-              </div>
-              <div className={style.Title}>
-                <h6>{title}</h6>
-              </div>
-              <div className={style.Extras}>
-                <h6>
-                  Respuestas:{cantAnswers} - T.Points:{teachPoints}
-                </h6>
-              </div>
+            <div className={style.userPreg}>
+              <h6>{nickname} pregunta:</h6>
             </div>
-            <div className={style.questionText}>
-              {text}
+            <div className={style.Title}>
+              <h6>{title}</h6>
             </div>
+            <div className={style.Extras}>
+              <h6>
+                Respuestas:{cantAnswers} - T.Points:{teachPoints}
+              </h6>
+            </div>
+          </div>
+          <div className={style.questionText}>{text}</div>
           <div className={style.bajoTexto}>
             <div className={style.likes}>
               {likeOnScreen}
-              <img src={like} alt="mano arriba" className={style.like}/>
-              <img src={dislike} alt="mano abajo" className={style.dislike}/>
+              <img src={like} alt="mano arriba" className={style.like} />
+              <img src={dislike} alt="mano abajo" className={style.dislike} />
             </div>
 
             <div>
-              <img src={favorito} alt="favorito" className={style.like}/>
+              <img src={favorito} alt="favorito" className={style.like} />
             </div>
             <div>
               <img src={denuncia} alt="denuncia" className={style.like} />
@@ -56,11 +60,9 @@ const CardQuestion = ({cantAnswers, nickname, picture, likes, title, text, teach
           </div>
         </div>
       </div>
-      <div className={`col-2-lg ${style.rightBox}`}>
-        <Link to={`/responder/${id}` }>
-          <button  className={style.answerIt}>
-              Responder
-          </button>
+      <div className={`col-lg-2 ${style.rightBox}`}>
+        <Link to={`/responder/${id}`}>
+          <button className={style.answerIt}>Responder</button>
         </Link>
       </div>
     </div>
