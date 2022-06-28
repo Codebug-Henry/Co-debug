e354946a4ac22e019483a623c9f29418be15d370
Matias <matibalbi@gmail.com>
2022-06-28 18:48:43 -0300

    creé modelo el Message para los mensajes que envían los usuarios a través del formulario de contacto y que reciben los admin, también armé las rutas y controladores de postMessage y getAllMessages, y actualicé el readme con todo esto nuevo

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,29 +1,29 @@
 const { Router } = require('express');
 
 const userRoutes = require("./userRoutes")
-
 const usersRoutes = require("./usersRoutes")
 
 const questionRoutes = require("./questionRoutes")
-
 const questionsRoutes = require("./questionsRoutes")
 
 const answerRoutes = require("./answerRoutes")
-
 const answersRoutes = require("./answersRoutes")
+
+const messageRoutes = require("./messageRoutes")
+const messagesRoutes = require("./messagesRoutes")
 
 const router = Router()
 
 router.use("/user", userRoutes)
-
 router.use("/users", usersRoutes)
 
 router.use("/question", questionRoutes)
-
 router.use("/questions", questionsRoutes)
 
 router.use("/answer", answerRoutes)
-
 router.use("/answers", answersRoutes)
+
+router.use("/message", messageRoutes)
+router.use("/messages", messagesRoutes)
 
 module.exports = router
