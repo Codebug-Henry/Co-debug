a447da0d47e248b16d1accfcabfe6f625a959175
Felipe Guitelmane <felipe.guitelman@.com>
2022-06-29 16:02:23 -0300

    se cambiaron variables d entorno

--- client/src/index.js
+++ client/src/index.js
@@ -7,16 +7,16 @@ import store from "./redux/store"
 import axios from 'axios';
 import { Auth0Provider } from "@auth0/auth0-react"
 require('dotenv').config();
-const {
-  REACT_APP_AUTH0_DOMAIN, REACT_APP_AUTH0_CLIENT_ID, REACT_APP_API
-} = process.env;
-const domain = "dev-n7q7v880.us.auth0.com";
-const clientId = "mc6ESq1wOdZwvUsxeVc2Zb41s1hwhrbx";
+// const {
+//   REACT_APP_AUTH0_DOMAIN, REACT_APP_AUTH0_CLIENT_ID, REACT_APP_API
+// } = process.env;
+const domain = process.env.REACT_APP_AUTH0_DOMAIN ;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID ;
 
 const container = document.getElementById("root")
 const root = ReactDOMClient.createRoot(container);
 
-axios.defaults.baseURL = REACT_APP_API || "http://localhost:3001"
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
 
 root.render(
   <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
