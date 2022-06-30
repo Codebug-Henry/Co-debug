e780054e8e5f00b4f1e955bd2432673508540f1a
XDrimer <tutemaposo2016@gmail.com>
2022-06-30 16:37:31 -0300

    solucion

--- client/src/components/adminComponents/AgregarAdmin.js
+++ client/src/components/adminComponents/AgregarAdmin.js
@@ -3,45 +3,45 @@ import style from "./styles/AgregarAdmin.module.css"
 
 const AgregarAdmin = () => {
 
-  const [input, setInput] = useState("")
+  // const [input, setInput] = useState("")
 
-  const onChangeSearch = (e) => {
-    setInput(e.target.value);
-    dispatch(getSearchQuestions(input, page));
-  };
+  // const onChangeSearch = (e) => {
+  //   setInput(e.target.value);
+  //   dispatch(getSearchQuestions(input, page));
+  // };
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    setPage(parseInt(e.target.value));
-  };
+  // const handleClick = (e) => {
+  //   e.preventDefault();
+  //   setPage(parseInt(e.target.value));
+  // };
 
-  return (
-    <>
-      <div className={style.adminList}>
+  // return (
+  //   <>
+  //     <div className={style.adminList}>
 
-      </div>
-      <div className={style.addAdmin}>
-          <div>
-              <form className="d-flex">
-                    <input
-                      onChange={(e) => onChangeSearch(e)}
-                      className="form-control me-2"
-                      type="search"
-                      placeholder="Buscar..."
-                      aria-label="Search"
-                    />
-                    <button
-                      onClick={() => handlerRefresh()}
-                      className="btn btn-outline-dark"
-                      type="submit"
-                    >
-                      Refresh
-                    </button>
-                  </form>
-          </div>
-      </div>
-    </>
-  )
+  //     </div>
+  //     <div className={style.addAdmin}>
+  //         <div>
+  //             <form className="d-flex">
+  //                   <input
+  //                     onChange={(e) => onChangeSearch(e)}
+  //                     className="form-control me-2"
+  //                     type="search"
+  //                     placeholder="Buscar..."
+  //                     aria-label="Search"
+  //                   />
+  //                   <button
+  //                     onClick={() => handlerRefresh()}
+  //                     className="btn btn-outline-dark"
+  //                     type="submit"
+  //                   >
+  //                     Refresh
+  //                   </button>
+  //                 </form>
+  //         </div>
+  //     </div>
+  //   </>
+  // )
 };
 
 export default AgregarAdmin;
