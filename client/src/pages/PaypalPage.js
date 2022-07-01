34083755a9f8eee2ba8312b09bb495243677df56
Regenerik <regenerik@gmail.com>
2022-07-01 14:44:35 -0300

    Paginaa de donación 1 - Terminada

--- client/src/pages/PaypalPage.js
+++ client/src/pages/PaypalPage.js
@@ -3,6 +3,8 @@ import Paypal from '../components/Paypal'
 import { useState } from 'react';
 import Footer from '../components/Footer.js';
 import style from './styles/PaypalPage.module.css'
+import supportdevelopers from '../images/supportdevelopers.png'
+import paypal from '../images/paypal2.png'
 
 const PaypalPage = () => {
 
@@ -13,18 +15,48 @@ const PaypalPage = () => {
     
     
     <div>
-
      { checkout ? (
-          <Paypal val={val} />
-      ) : (
-        <div>
-          {/* // <button onClick={()=>{setCheckOut(true)}}>
-          //   Checkout
-          // </button> */}
-          <input onChange={(e)=>setVal(parseInt(e.target.value))} ></input>
 
-          <button onClick={()=> setCheckOut(true)}>Click para setear monto a donar</button>
+
+          <Paypal val={val} />
+
+
+
+      ) : (
+
+      <div className={style.BigPaypalOne}>
+        <div className={style.tituloYTexto}>
+          <p className={style.titulo}>
+            Invirtiendo en Mejoras
+          </p>
+          <p>
+            Primeramente queremos agradecerte por considerar ayudar en nuestro proyecto. Nos parece justo contarte un poco para que será utilizada tu donación.
+            Este es un proyecto totalmente gratuito para todo usuario que lo necesite. Independientemente de eso, las mejoras y desarrollo de esta herramienta conlleva muchas horas de trabajo.
+            Queremos poder invertir ese tiempo y el dinero que nos facilitas para poder mejorar todos los aspectos de este sitio.
+            Desde ya estamos agradecidos por tenerte en esta sección.
+
+
+          </p>
+        </div>
+
+
+          <div className={style.BoxPaypalOne}>
+            <div className={style.paypalArea}>
+              <p>Powered By</p>
+              <img className={style.paypalLogo} src={paypal}/>
+            </div>
+            <div className={style.setValor}>
+              <p className={style.plata}>R$ </p>
+              <input className={style.input} onChange={(e)=>setVal(parseInt(e.target.value))} ></input>
+              <button onClick={()=> setCheckOut(true)}>Donar</button>
+            </div>
           </div>
+
+        <div className={style.sdevelopers}>
+          <img src={supportdevelopers}></img>
+        </div>
+      </div>
+
         ) 
       }
       <div className={style.footer}>
