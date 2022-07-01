8b47d4ea53265699e6a3cabe7bcda6a27d772ca9
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-07-01 18:44:17 -0300

    Respuesta a consultas directas por nodemailer

--- client/src/components/adminComponents/PreguntasDirectas.js
+++ client/src/components/adminComponents/PreguntasDirectas.js
@@ -1,41 +1,40 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Message from "./Message";
-import style from "./styles/PreguntasDirectas.module.css"
+import style from "./styles/PreguntasDirectas.module.css";
 import Paginated from "../Paginated";
 
-const PreguntasDirectas = ({messagePage, setMessagePage}) => {
-
-  const messages = useSelector((state)=>state.messages)
-  const totalPages = useSelector((state)=>state.totalPages)
+const PreguntasDirectas = ({ messagePage, setMessagePage }) => {
+  const messages = useSelector((state) => state.messages);
+  const totalPages = useSelector((state) => state.totalPages);
 
   return (
     <div className={style.container}>
-        <div>
-        {
-          messages && messages.map((e)=>{
+      <div>
+        {messages &&
+          messages.map((e) => {
             return (
               <Message
-              title={e.title}
-              text={e.text}
-              email={e.user.email}
-              sub={e.userSub}
-              nickname={e.user.nickname}
+                key={e.id}
+                title={e.title}
+                text={e.text}
+                email={e.user.email}
+                sub={e.userSub}
+                nickname={e.user.nickname}
+                id={e.id}
               />
-            )
-          })
-        }
-        </div>
-        <div>
-          <Paginated
+            );
+          })}
+      </div>
+      <div>
+        <Paginated
           page={messagePage}
           setPage={setMessagePage}
           totalPages={totalPages}
-          />
-        </div>
+        />
+      </div>
     </div>
-
-  )
+  );
 };
 
 export default PreguntasDirectas;
