eb5253754f881f41fab3b34aa10ab43088b7333a
GonzaloGDV <69487420+GonzaloGDV@users.noreply.github.com>
2022-07-02 16:30:44 -0300

    Update PaypalPage.js

--- client/src/pages/PaypalPage.js
+++ client/src/pages/PaypalPage.js
@@ -1,18 +1,20 @@
-import React from "react";
-// import Paypal from '../components/Paypal'
-import { useState } from "react";
-import Footer from "../components/Footer.js";
-import style from "./styles/PaypalPage.module.css";
-import supportdevelopers from "../images/supportdevelopers.png";
-import paypal from "../images/paypal2.png";
+import React from 'react'
+import Paypal from '../components/Paypal'
+import { useState } from 'react';
+import Footer from '../components/Footer.js';
+import style from './styles/PaypalPage.module.css'
+import supportdevelopers from '../images/supportdevelopers.png'
+import paypal from '../images/paypal2.png'
 
 const PaypalPage = () => {
-  const [val, setVal] = useState(1);
-  const [checkout, setCheckOut] = useState(false);
+
+  const [ val, setVal ] = useState(1)
+  const [ checkout, setCheckOut ] = useState(false);
 
   return (
+    
+    
     <div>
-
      { checkout ? (
         <div>
           <div >
@@ -48,29 +50,22 @@ const PaypalPage = () => {
           <p>
             Invirtiendo en Mejoras
           </p>
-
           <p>
-            Primeramente queremos agradecerte por considerar ayudar en nuestro
-            proyecto. Nos parece justo contarte un poco para que será utilizada
-            tu donación. Este es un proyecto totalmente gratuito para todo
-            usuario que lo necesite. Independientemente de eso, las mejoras y
-            desarrollo de esta herramienta conlleva muchas horas de trabajo.
-            Queremos poder invertir ese tiempo y el dinero que nos facilitas
-            para poder mejorar todos los aspectos de este sitio. Desde ya
-            estamos agradecidos por tenerte en esta sección.
+            Primeramente queremos agradecerte por considerar ayudar en nuestro proyecto. Nos parece justo contarte un poco para que será utilizada tu donación.
+            Este es un proyecto totalmente gratuito para todo usuario que lo necesite. Independientemente de eso, las mejoras y desarrollo de esta herramienta conlleva muchas horas de trabajo.
+            Queremos poder invertir ese tiempo y el dinero que nos facilitas para poder mejorar todos los aspectos de este sitio.
+            Desde ya estamos agradecidos por tenerte en esta sección.
           </p>
         </div>
         <div >
           <div>
             <p>Powered By</p>
-
             <img/>
           </div>
           <div>
             <p >R$ </p>
             <input  onChange={(e)=>setVal(parseInt(e.target.value))} ></input>
             <button onClick={()=> setCheckOut(true)}>Donar</button>
-
           </div>
         </div>
         <div >
@@ -78,15 +73,16 @@ const PaypalPage = () => {
         </div>
       </div>
 
-
          ) 
       }
       <div>
-
         <Footer />
       </div>
+      
     </div>
-  );
-};
 
-export default PaypalPage;
+
+  );
+}
+
+export default PaypalPage
