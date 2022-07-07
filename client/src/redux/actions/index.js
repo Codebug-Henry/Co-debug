e12d69964f2d4e8ee674f86d2edae66fc17c2ece
Matias <matibalbi@gmail.com>
2022-07-07 12:00:30 -0300

    prueba solucion rotura de likes

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -152,12 +152,13 @@ export const getQuestion = (id, setLoad) => async (dispatch) => {
   }
 };
 
-export const modifyQuestion = (question, setIsModify, setIsLiked) => async (dispatch) => {
+export const modifyQuestion = (question, setIsModify, setIsLiked, setFlag) => async (dispatch) => {
   try {
     const { data } = await api.modifyQuestion(question);
     dispatch({ type: MODIFY_QUESTION, payload: data });
     setIsModify && setIsModify(false);
     setIsLiked && setIsLiked(prevIsLiked => !prevIsLiked)
+    setFlag && setFlag(true)
   } catch (error) {
     console.log(error.message);
   }
