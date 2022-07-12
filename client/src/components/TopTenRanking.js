78e7a4e4ffcfc920947415b57cdca3924e0ea3d1
Luciano <lucianommorea@gmail.com>
2022-07-12 16:23:50 -0300

    Estilos TopTen, arreglo Ranking y setPage1 en filtrados Ranking

--- client/src/components/TopTenRanking.js
+++ client/src/components/TopTenRanking.js
@@ -4,6 +4,9 @@ import style from "./styles/TopTenRanking.module.css";
 import { getTopTenRanking } from "../redux/actions";
 import { useEffect } from "react";
 import TopTenCard from "./TopTenCard";
+import oro from '../images/oro.png';
+import plata from '../images/plata.png';
+import bronce from '../images/bronce.png';
 
 const TopTenRanking = () => {
   const dispatch = useDispatch();
@@ -24,23 +27,64 @@ const TopTenRanking = () => {
           </div>
         </div>
 
-        {topTen ? (
+        {topTen && (
           <>
-            {topTen?.map((e) => {
-              return (
+            {topTen?.map((e) => e.myPosition === 1 ? 
+                <div className={`row ${style.top1} `} key={e.sub}>
+                  <TopTenCard
+                    position= {<img src={oro} className={style.medal} alt="1" />}
+                    nickname={e.nickname}
+                    points={e.myTeachPoints}
+                  />
+                </div>
+             : null )}
+          </>
+        )}
+
+        {topTen && (
+          <>
+            {topTen?.map((e) => e.myPosition === 2 ? 
+                <div className={`row ${style.top2} `} key={e.sub}>
+                  <TopTenCard
+                    position= {<img src={plata} className={style.medal} alt="2" />}
+                    nickname={e.nickname}
+                    points={e.myTeachPoints}
+                  />
+                </div>
+             : null )}
+          </>
+        )}
+
+        {topTen && (
+          <>
+            {topTen?.map((e) => e.myPosition === 3 ? 
+                <div className={`row ${style.top3} `} key={e.sub}>
+                  <TopTenCard
+                    position= {<img src={bronce} className={style.medal} alt="3" />}
+                    nickname={e.nickname}
+                    points={e.myTeachPoints}
+                  />
+                </div>
+             : null )}
+          </>
+        )}
+        
+
+        {topTen ?
+          <>
+            {topTen?.map((e) => e.myPosition > 3 ?
                 <div className={`row ${style.mapeando} `} key={e.sub}>
                   <TopTenCard
                     position={e.myPosition}
                     nickname={e.nickname}
                     points={e.myTeachPoints}
                   />
-                </div>
-              );
-            })}
-          </>
-        ) : (
-          <div>No se encontraron usuarios</div>
-        )}
+                </div> 
+              : null
+            )}
+          </> :
+          <div> No se encontraron usuarios </div>
+        }
       </div>
     </div>
   );
