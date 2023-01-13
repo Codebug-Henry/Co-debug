a7483c80f6d3ef535719ebea412471cf728fb149
Gonzalo Gomez <gonzalogdv@gmail.com>
2023-01-13 14:16:37 -0300

    Map &&

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,24 @@
-import React, {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import { getAllQuestions, getAllTags, getSearchQuestions,setMacrotag, setMicrotag, setSort, setSortValidate } from "../redux/actions/index.js";
-import style from "./styles/NavBar.module.css";
-import { TextField, FormControl, Select, MenuItem, InputLabel } from "@mui/material";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getAllQuestions,
+  getAllTags,
+  getSearchQuestions,
+  setMacrotag,
+  setMicrotag,
+  setSort,
+  setSortValidate,
+} from '../redux/actions/index.js';
+import style from './styles/NavBar.module.css';
+import {
+  TextField,
+  FormControl,
+  Select,
+  MenuItem,
+  InputLabel,
+} from '@mui/material';
 
-const NavBar = ({search, setSearch, setPage}) => {
+const NavBar = ({ search, setSearch, setPage }) => {
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.sort);
   const validated = useSelector((state) => state.sortValidate);
@@ -16,11 +30,12 @@ const NavBar = ({search, setSearch, setPage}) => {
     dispatch(getAllTags());
   }, [dispatch]);
 
-
   const onChangeSearch = (e) => {
     setSearch(e.target.value);
-    setPage(1) 
-    dispatch(getSearchQuestions(e.target.value, sort, 1, validated, macroTag, microTag));
+    setPage(1);
+    dispatch(
+      getSearchQuestions(e.target.value, sort, 1, validated, macroTag, microTag)
+    );
   };
 
   const handlerRefresh = () => {
@@ -28,10 +43,19 @@ const NavBar = ({search, setSearch, setPage}) => {
   };
 
   const handleSort = (e) => {
-    setPage(1) 
+    setPage(1);
     dispatch(setSort(e.target.value));
     if (search.length > 0) {
-      dispatch(getSearchQuestions(search, e.target.value, 1, validated, macroTag, microTag));
+      dispatch(
+        getSearchQuestions(
+          search,
+          e.target.value,
+          1,
+          validated,
+          macroTag,
+          microTag
+        )
+      );
     } else {
       dispatch(
         getAllQuestions(e.target.value, 1, validated, macroTag, microTag)
@@ -40,104 +64,128 @@ const NavBar = ({search, setSearch, setPage}) => {
   };
 
   const handleSortValidate = (e) => {
-    setPage(1) 
+    setPage(1);
     dispatch(setSortValidate(e.target.value));
     if (search.length > 0) {
       dispatch(
-        getSearchQuestions(search, sort, 1, e.target.value, macroTag, microTag));
+        getSearchQuestions(search, sort, 1, e.target.value, macroTag, microTag)
+      );
     } else {
       dispatch(getAllQuestions(sort, 1, e.target.value, macroTag, microTag));
     }
   };
 
   const handleMacroTag = (e) => {
-    setPage(1) 
+    setPage(1);
     dispatch(setMacrotag(e.target.value));
-    dispatch(setMicrotag("All"));
+    dispatch(setMicrotag('All'));
     if (search.length > 0) {
       dispatch(
-        getSearchQuestions(search, sort, 1, validated, e.target.value, "All")
+        getSearchQuestions(search, sort, 1, validated, e.target.value, 'All')
       );
     } else {
-      dispatch(getAllQuestions(sort, 1, validated, e.target.value, "All"));
+      dispatch(getAllQuestions(sort, 1, validated, e.target.value, 'All'));
     }
   };
 
   const handleMicroTag = (e) => {
-    setPage(1) 
+    setPage(1);
     dispatch(setMicrotag(e.target.value));
     if (search.length > 0) {
       dispatch(
-        getSearchQuestions(search, sort, 1, validated, macroTag, e.target.value));
-    } else {
-      dispatch(
-        getAllQuestions(sort, 1, validated, macroTag, e.target.value)
+        getSearchQuestions(search, sort, 1, validated, macroTag, e.target.value)
       );
+    } else {
+      dispatch(getAllQuestions(sort, 1, validated, macroTag, e.target.value));
     }
   };
 
   return (
     <div className={`container-fluid ${style.optionSearch}`}>
-      <nav className={`navbar navbar-expand-lg navbar-light bg-warning ${style.navbar}`} >
-        <div className="container-fluid">
+      <nav
+        className={`navbar navbar-expand-lg navbar-light bg-warning ${style.navbar}`}
+      >
+        <div className='container-fluid'>
           <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className='navbar-toggler-icon'></span>
           </button>
 
           <div className={`d-flex ${style.navSearch}`}>
-            <TextField id="outlined-basic" onChange={onChangeSearch} type="search" label="Buscar..." variant="outlined"/>
+            <TextField
+              id='outlined-basic'
+              onChange={onChangeSearch}
+              type='search'
+              label='Buscar...'
+              variant='outlined'
+            />
           </div>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul className={`navbar-nav me-auto mb-2 mb-lg-0 ${style.ul}`}>
-              <FormControl sx={{width: 140, margin: 0.5}}>
-                <InputLabel id="demo-simple-select-label">Creación</InputLabel>
-                <Select value={sort} label="Creacion" onChange={handleSort}>
-                  <MenuItem value="desc">Más nuevas</MenuItem>
-                  <MenuItem value="asc">Más antiguas</MenuItem>
+              <FormControl sx={{ width: 140, margin: 0.5 }}>
+                <InputLabel id='demo-simple-select-label'>Creación</InputLabel>
+                <Select value={sort} label='Creacion' onChange={handleSort}>
+                  <MenuItem value='desc'>Más nuevas</MenuItem>
+                  <MenuItem value='asc'>Más antiguas</MenuItem>
                 </Select>
               </FormControl>
 
-              <FormControl sx={{width: 140, margin: 0.5}}>
+              <FormControl sx={{ width: 140, margin: 0.5 }}>
                 <InputLabel>Validadas</InputLabel>
-                <Select value={validated} label="Validadas" onChange={handleSortValidate}>
-                  <MenuItem value="All">Todas</MenuItem>
-                  <MenuItem value="true">Validadas</MenuItem>
-                  <MenuItem value="false">No validadas</MenuItem>
+                <Select
+                  value={validated}
+                  label='Validadas'
+                  onChange={handleSortValidate}
+                >
+                  <MenuItem value='All'>Todas</MenuItem>
+                  <MenuItem value='true'>Validadas</MenuItem>
+                  <MenuItem value='false'>No validadas</MenuItem>
                 </Select>
               </FormControl>
 
-              <FormControl sx={{width: 140, margin: 0.5}}>
+              <FormControl sx={{ width: 140, margin: 0.5 }}>
                 <InputLabel>Macrotag</InputLabel>
-                <Select value={macroTag} label="Macrotag" onChange={handleMacroTag}>
-                  <MenuItem value="All">Todos</MenuItem>
-                  {tags?.map((e) => (
-                    <MenuItem key={e.id} value={e.tag}>
-                      {e.tag}
-                    </MenuItem>
-                  ))}
+                <Select
+                  value={macroTag}
+                  label='Macrotag'
+                  onChange={handleMacroTag}
+                >
+                  <MenuItem value='All'>Todos</MenuItem>
+                  {tags &&
+                    tags.map((e) => (
+                      <MenuItem key={e.id} value={e.tag}>
+                        {e.tag}
+                      </MenuItem>
+                    ))}
                 </Select>
               </FormControl>
 
-              <FormControl sx={{width: 140, margin: 0.5}}>
+              <FormControl sx={{ width: 140, margin: 0.5 }}>
                 <InputLabel>Microtag</InputLabel>
-                <Select value={microTag} label="Microtag" onChange={handleMicroTag}>
-                  <MenuItem value="All">Todos</MenuItem>
-                  {macroTag !== "All"
-                    ? tags.filter((e) => e.tag === macroTag).flatMap((e) => e.microTags).map((e) =>
-                        (
+                <Select
+                  value={microTag}
+                  label='Microtag'
+                  onChange={handleMicroTag}
+                >
+                  <MenuItem value='All'>Todos</MenuItem>
+                  {macroTag !== 'All'
+                    ? tags
+                        .filter((e) => e.tag === macroTag)
+                        .flatMap((e) => e.microTags)
+                        .map((e) => (
                           <MenuItem key={e.id} value={e.tag}>
                             {e.tag}
                           </MenuItem>
-                        )) : ""}
+                        ))
+                    : ''}
                 </Select>
               </FormControl>
             </ul>
@@ -145,7 +193,7 @@ const NavBar = ({search, setSearch, setPage}) => {
             <button
               onClick={() => handlerRefresh()}
               className={`btn btn-outline-dark ${style.refresh}`}
-              type="button"
+              type='button'
             >
               Refresh
             </button>
